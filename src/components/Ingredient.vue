884d348cd69ed2f92aa1cb55dbb3e319b1a625ea
Knudz <kasperknudz@gmail.com>
2019-12-11 13:55:35 +0100

    Ändringar

--- src/components/Ingredient.vue
+++ src/components/Ingredient.vue
@@ -1,36 +1,72 @@
 <template>
-  <div class="ingredient">
-    <label>
-      <button v-on:click="incrementCounter">{{ counter }}</button>
-      {{item["ingredient_"+ lang]}}, {{item.selling_price}}:-, {{item.stock}} pcs
-    </label>
+<div class="ingredient">
+  <label>
+
+    {{item["ingredient_"+ lang]}}, {{item.selling_price}}:-, {{item.stock}} pcs
+  </label>
+  <div class="gridBackground">
+    <button v-if="moreThanZeroOrders===false" v-on:click="decreaseCounter">
+      <img v-on:click: src="@/assets/minus_sign.png" width=25px;>
+    </button>
+
+    {{ this.counter }}
+
+    <button v-on:click="incrementCounter">
+      <img v-on:click: src="@/assets/plus_sign.jpg" width=25px;>
+    </button>
   </div>
+
+
+</div>
 </template>
 <script>
 export default {
   name: 'Ingredient',
   props: {
     item: Object,
-    lang: String
+    lang: String,
+    orderDoesNotExist: Boolean,
+    moreThanZeroOrders: Boolean
+
   },
-    data: function () {
+  data: function() {
     return {
-      counter: 0
+      counter: 0,
+
     };
   },
   methods: {
-    incrementCounter: function () {
+    incrementCounter: function() {
+
+
       this.counter += 1;
       // sending 'increment' message to parent component or view so that it
       // can catch it with v-on:increment in the component declaration
       this.$emit('increment');
+
     },
-    resetCounter: function () {
+    decreaseCounter: function() {
+      if (this.counter > 0) {
+        this.counter -= 1;
+        this.$emit('decrease');
+      }
+    },
+    resetCounter: function() {
       this.counter = 0;
     }
   }
 }
 </script>
 <style scoped>
-  
+.gridBackground {
+  margin-right: 0px;
+  color: white;
+  background-image: url('~@/assets/exampleImage.jpg');
+
+
+}
+
+#counterBackground {
+
+}
 </style>
--- src/views/Ordering.vue
+++ src/views/Ordering.vue
@@ -1,39 +1,38 @@
 <template>
-  <div id="ordering">
-    <img class="example-panel" src="@/assets/exampleImage.jpg">
-    <button v-on:click="switchLang()">{{ uiLabels.language }}</button>
+<div id="ordering">
 
-    <h1>{{ uiLabels.ingredients }}</h1>
+  <button v-on:click="switchLang()">{{ uiLabels.language }}</button>
 
-    <Ingredient
-      ref="ingredient"
-      v-for="item in ingredients"
-      v-on:increment="addToOrder(item)"  
-      :item="item" 
-      :lang="lang"
-      :key="item.ingredient_id">
-    </Ingredient>
+  <h1 id="header"> Välkomna till skurknästet </h1>
 
-    <h1>{{ uiLabels.order }}</h1>
-    {{ chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}, {{ price }} kr
-    <button v-on:click="placeOrder()">{{ uiLabels.placeOrder }}</button>
-
-    <h1>{{ uiLabels.ordersInQueue }}</h1>
-    <div>
-      <OrderItem 
-        v-for="(order, key) in orders"
-        v-if="order.status !== 'done'"
-        :order-id="key"
-        :order="order" 
-        :ui-labels="uiLabels"
-        :lang="lang"
-        :key="key">
-      </OrderItem>
-    </div>
+  <div class="hamburgerIngredients">
+    <div class="grid-button-item-1"><button v-on:click="changeCategory(1)"> KÖTT </button> </div>
+    <div class="grid-button-item-1"><button v-on:click="changeCategory(2)"> PÅLÄGG </button></div>
+    <div class="grid-button-item-2"> <button v-on:click="changeCategory(3)"> SÅS </button></div>
+    <div class="grid-button-item-2"> <button v-on:click="changeCategory(4)"> BRÖD </button></div>
   </div>
+
+  <h1>{{ uiLabels.ingredients }}</h1>
+
+  <div id="gridInner">
+    <Ingredient ref="ingredient" v-for="item in ingredients" v-on:increment="addToOrder(item)" v-on:decrease="reduceOrder(item)" :item="item" :categoryNumber="currentCategory" :lang="lang" :key="item.ingredient_id">
+    </Ingredient>
+  </div>
+
+  <div id="test">
+    <h1>{{ uiLabels.order }}</h1> {{ chosenIngredients.map(item => item["ingredient_"+lang]).join(', ') }}, {{ price }} kr
+    <button v-on:click="placeOrder()"> {{ uiLabels.placeOrder }} </button>
+  </div>
+  <h1>{{ uiLabels.ordersInQueue }}</h1>
+  <div>
+    <OrderItem v-for="(order, key) in orders" v-if="order.status !== 'done'" :order-id="key" :order="order" :ui-labels="uiLabels" :lang="lang" :key="key">
+    </OrderItem>
+  </div>
+
+
+</div>
 </template>
 <script>
-
 //import the components that are used in the template, the name that you
 //use for importing will be used in the template above and also below in
 //components
@@ -43,7 +42,7 @@ import OrderItem from '@/components/OrderItem.vue'
 //import methods and data that are shared between ordering and kitchen views
 import sharedVueStuff from '@/components/sharedVueStuff.js'
 
-/* instead of defining a Vue instance, export default allows the only 
+/* instead of defining a Vue instance, export default allows the only
 necessary Vue instance (found in main.js) to import your data and methods */
 export default {
   name: 'Ordering',
@@ -51,34 +50,58 @@ export default {
     Ingredient,
     OrderItem
   },
-  mixins: [sharedVueStuff], // include stuff that is used in both 
-                            // the ordering system and the kitchen
+  mixins: [sharedVueStuff], // include stuff that is used in both
+  // the ordering system and the kitchen
   data: function() { //Not that data is a function!
     return {
       chosenIngredients: [],
       price: 0,
       orderNumber: "",
+      currentCategory: 1,
+      hamburgerButtons: false,
+      displayOrder: false
     }
   },
-  created: function () {
-    this.$store.state.socket.on('orderNumber', function (data) {
+  created: function() {
+    this.$store.state.socket.on('orderNumber', function(data) {
       this.orderNumber = data;
     }.bind(this));
   },
   methods: {
-    addToOrder: function (item) {
+    showBurger: function(boolean) {
+      this.hamburgerButtons = boolean;
+    },
+
+    showOrder: function(boolean) {
+      this.displayOrder = boolean;
+    },
+
+    changeCategory: function(int) {
+      this.currentCategory = int;
+    },
+
+    addToOrder: function(item) {
       this.chosenIngredients.push(item);
       this.price += +item.selling_price;
     },
-    placeOrder: function () {
+
+    reduceOrder: function(item) {
+      this.chosenIngredients.splice(item, 1);
+      this.price += -item.selling_price;
+    },
+
+    placeOrder: function() {
       var i,
-      //Wrap the order in an object
+        //Wrap the order in an object
         order = {
           ingredients: this.chosenIngredients,
           price: this.price
+
         };
       // make use of socket.io's magic to send the stuff to the kitchen via the server (app.js)
-      this.$store.state.socket.emit('order', {order: order});
+      this.$store.state.socket.emit('order', {
+        order: order
+      });
       //set all counters to 0. Notice the use of $refs
       for (i = 0; i < this.$refs.ingredient.length; i += 1) {
         this.$refs.ingredient[i].resetCounter();
@@ -91,21 +114,82 @@ export default {
 </script>
 <style scoped>
 /* scoped in the style tag means that these rules will only apply to elements, classes and ids in this template and no other templates. */
+
 #ordering {
-  margin:auto;
   width: 40em;
+}
+
+#header {
+  color: green;
+  font-family: "Trebuchet MS", Helvetica, sans-serif;
+  font-size: 50px;
+  font-style: italic;
+  position: relative;
+  left: 450px;
+}
+
+#gridInner {
+  display: grid;
+  grid-gap: 5px;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: auto auto auto auto;
+  justify-content: space-evenly;
+}
+
+#test {
+  position: relative;
+  left: 790px;
+  bottom: 1600px;
+  border: 3px solid #73AD21;
+  color: green;
+  font-family: "Trebuchet MS", Helvetica, sans-serif;
 }
 
 .example-panel {
   position: fixed;
-  left:0;
-  top:0;
-  z-index: -2;
-}
-.ingredient {
-  border: 1px solid #ccd;
-  padding: 1em;
   background-image: url('~@/assets/exampleImage.jpg');
+  width: 200px;
+  left: 0;
+  top: 0;
+  z-index: 2;
+}
+.hamburgerIngredients {
+display: grid;
+grid-template-columns: auto auto auto auto;
+}
+
+.hamburgerIngredients button {
+  color: #FFFFFF;
+  opacity: 0.9;
+  width: 200px;
+  padding: 50px;
+  border-radius: 20px;
+  background-color: black;
+}
+
+.hamburgerIngredients button:hover {
+  background-color: green;
   color: white;
+}
+
+.grid-button-item-1 {
+  grid-row: 1;
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+
+.grid-button-item-2 {
+  grid-row: 2;
+}
+
+.ingredient {
+  border: 3px solid #ccd;
+  padding: 10px;
+  color: white;
+  font-family: "Trebuchet MS", Helvetica, sans-serif;
+  background-image: url('~@/assets/exampleImage.jpg');
 }
 </style>
